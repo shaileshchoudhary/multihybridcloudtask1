ca016b1ff90c79134bc40ad92d7a523c1b2c526d
shaileshchoudhary <43171769+shaileshchoudhary@users.noreply.github.com>
2020-06-13 10:20:08 +0530

    werr

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ print 'ifconfig'
 print '</br>'
 print 'free -m'
   
-<img src='d1m2fz859hqifk.cloudfront.net' width='400' height='400' />
+<img src='d2amrrsqi9s1pt.cloudfront.net' width='400' height='400' />
   
  print '</br>'
 
